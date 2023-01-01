f61bd125339aa2e53fdbe3995af9023db1587060
kalai <im.kalaiselvan.t@gmail.com>
2023-01-01 21:54:21 +0100

    stopping at signal

--- basic_agent_st/starting_point.cc
+++ basic_agent_st/starting_point.cc
@@ -7,6 +7,7 @@
 #include <math.h>
 #include <vector>
 #include <algorithm>
+#include <numeric>
 //#include "codegen/lib/pass_primitive/pass_primitive.h"
 #include "codegen/lib/primitives/primitives.h"
 #include "codegen/lib/v_opt/v_opt.h"
@@ -34,16 +35,23 @@ double x_in = 10.0;
 double x_tr,x_stop;
 double coeffsT1[6];
 double coeffsT2[6];
+double best_coeff[6];
+double stop_coeff[6];
+double j0_coeff[6];
 
 double x_s = 5.0;
+double max_sf;
+double t_f;
+double vfj0,tfj0;
 
 double v_r = 8.0;
 double V_min = 3.0;
 double V_max = 15.0;
 
-double T_s = x_s / V_min;
-double T_in = x_in / V_min;
+double t_s = x_s / V_min;
+double t_in = x_in / V_min;
 
+double t_green,t_red;
 
 
 // Handler for CTRL-C
@@ -55,16 +63,14 @@ void intHandler(int signal) {
 
 double v1,v2,T1,T2;
 
-
-
 double low_level_ctrl
         (double vel, double acc, double minAcc, double maxAcc)
 {
     static double old_req_acc = 0.0;
     double long_gain = 1.0;
-    double j0 = coeffsT1[3];
-    double s0 = coeffsT1[4];
-    double cr0 = coeffsT1[5];
+    double j0 = best_coeff[3];
+    double s0 = best_coeff[4];
+    double cr0 = best_coeff[5];
     double toffs = 0.0;
     double jT0 = j0 + toffs * s0 + 0.5 * toffs * toffs * cr0;
     double jT1 = j0 + (DT+toffs)*s0 + 0.5*(DT*toffs)*(DT*toffs)*cr0;
@@ -152,6 +158,78 @@ int main(int argc, const char * argv[]) {
             if(in->TrfLightDist == 0.0 || x_tr > lookhead)
             {
                 freeflow(a_0,v_0,in->TrfLightDist,v_r);
+                for (int i = 0; i < 6; ++i)
+                {
+                    best_coeff[i] = coeffsT2[i];
+                }
+            }
+            else
+            {
+                switch(in->TrfLightCurrState)
+                {
+                    case 1:
+                        t_green = 0;
+                        t_red = in->TrfLightFirstTimeToChange - t_in;
+                        break;
+                    case 2:
+                        t_green = in->TrfLightSecondTimeToChange + t_s;
+                        t_red = in->TrfLightThirdTimeToChange - t_in;
+                        break;
+                    case 3:
+                        t_green = in->TrfLightFirstTimeToChange + t_s;
+                        t_red = in->TrfLightSecondTimeToChange - t_in;
+                        break;
+                }
+                if(in->TrfLightCurrState == 1 && in->TrfLightDist <= x_s)
+                {
+                    freeflow(v_0,a_0,lookhead,v_r);
+                    for (int i = 0; i < 6; ++i)
+                    {
+                        best_coeff[i] = coeffsT2[i];
+                    }
+                }
+                else
+                {
+                    pass_primitive(v_0,a_0,x_tr,V_min,V_max,t_green,t_red,coeffsT2,&v2,&T2,coeffsT1,&v1,&T1);
+                    double sum1 = std::accumulate(std::begin(coeffsT1),std::end(coeffsT1),0.0,std::plus<double>());
+                    double sum2 = std::accumulate(std::begin(coeffsT2),std::end(coeffsT2),0.0,std::plus<double>());
+                    if(sum1 == 0.0 && sum2 == 0.0)
+                    {
+                        stop_primitive(v_0,a_0,x_stop,stop_coeff,&max_sf,&t_f);
+                        for (int i = 0; i < 6; ++i)
+                        {
+                            best_coeff[i] = stop_coeff[i];
+                        }
+                    }
+                    else
+                    {
+                        if ((coeffsT1[2] < 0.0 && coeffsT2[2] > 0.0) || (coeffsT1[2] > 0.0 && coeffsT2[2] < 0.0))
+                        {
+                            pass_primitive_j0(v_0,a_0,x_tr,V_min,V_max,j0_coeff,&vfj0,&tfj0);
+                            for (int i = 0; i < 6; ++i)
+                            {
+                                best_coeff[i] = j0_coeff[i];
+                            }
+                        }
+                        else
+                        {
+                            if (std::abs(coeffsT1[2]) < std::abs(coeffsT2[2]))
+                            {
+                                for (int i = 0; i < 6; ++i)
+                                {
+                                    best_coeff[i] = coeffsT1[i];
+                                }
+                            }
+                            else
+                            {
+                                for (int i = 0; i < 6; ++i)
+                                {
+                                    best_coeff[i] = coeffsT2[i];
+                                }
+                            }
+                        }
+                    }
+                }
             }
 
             // Example of using log
